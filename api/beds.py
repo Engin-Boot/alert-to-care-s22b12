cdc18007df0f2324adbf8b83ff665547cd6bb7d6
Tanmoy Debnath <Tanmoy.Debnath@philips.com>
2020-10-09 11:22:32 +0530

    workflow

--- api/beds.py
+++
@@ -1,6 +0,0 @@
-class Beds:
-    def __init__(self,vacant,id):
-        self.vacant=vacant;
-        self.id=id;
-
-    
--- api/patients.py
+++
@@ -1,8 +0,0 @@
-class Patient:
-	def __init__(self,name,age,bedid,spo2,bp,heartbeat):
-		self.name=name;
-		self.age=age;
-		self.bedid=bedid;
-		self.spo2=spo2;
-		self.bp=bp;
-		self.heartbeat=heartbeat;
