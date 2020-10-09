a671e881c224d8feebb6a4ba5f09f8ec52a78344
Tanmoy Debnath <Tanmoy.Debnath@philips.com>
2020-10-09 17:16:36 +0530

    check workflow

--- src/test_app.py
+++ src/test_app.py
@@ -3,7 +3,7 @@ import app
 
 class Test(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(app.hello_world(),'hello world')
+        self.assertEqual(app.hello_world(),'helllo world')
         
 
 if __name__ == '__main__' :
