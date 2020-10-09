aa527801fe0261eba396ec1da6b78015fd33fcc6
Tanmoy Debnath <Tanmoy.Debnath@philips.com>
2020-10-09 11:31:39 +0530

    api

---
+++ api/patients.py
@@ -0,0 +1,8 @@
+class Patient:
+	def __init__(self,name,age,bedid,spo2,bp,heartbeat):
+		self.name=name;
+		self.age=age;
+		self.bedid=bedid;
+		self.spo2=spo2;
+		self.bp=bp;
+		self.heartbeat=heartbeat;
