6582c17c1b65bb528b2711d88e8e79d70bb49bcc
Tanmoy Debnath <Tanmoy.Debnath@philips.com>
2020-10-11 22:17:44 +0530

    solved lint errors

--- api/api.py
+++ api/api.py
@@ -16,6 +16,7 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
+# pylint: disable-all
 @app.route('/addBeds',methods=['POST'])
 def addBedsRepository():
     numberofbeds=request.json['numberofbeds']
