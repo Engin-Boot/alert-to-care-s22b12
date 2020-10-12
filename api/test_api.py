70c8fdde16cbd7de71f44bb381f5a9fe29efb3b0
mayank-ranjan17 <48884605+mayank-ranjan17@users.noreply.github.com>
2020-10-12 17:06:37 +0530

    Update test_api.py

--- api/test_api.py
+++ api/test_api.py
@@ -5,35 +5,32 @@ from bedsrepository import BedsRepositry
 
 class Test(unittest.TestCase):
     def test_add_beds_repository(self):
-        data = {"numberofbeds" : "10"}
+        data = {"numberofbeds": "10"}
         numberofbeds = data["numberofbeds"]
-        response=BedsRepositry.addBeds(numberofbeds)
+        response = BedsRepositry.addBeds(numberofbeds)
         expected = "10 beds are added"
-        self.assertEqual(response,expected)
-    
+        self.assertEqual(response, expected)
+
     def test_patient_entry(self):
-        data = {"name": "Suresh",
-        "age" : "70"}
-        patientdetails=Patient(data['name'],data['age']);
-        response=patientrepositry.addPatient(patientdetails)
+        data = {"name": "Suresh", "age": "70"}
+        patientdetails = Patient(data['name'],data['age']);
+        response = patientrepositry.addPatient(patientdetails)
         expected = "Bed number 1 is now occupied by Suresh"
-        self.assertEqual(response,expected)
-    
-    def test_check_vitals(self):
-        
-        response=patientrepositry.patientCheckVitals()
-        # expected = "Check patient on bed number 1 for following issues Low SPO2 level \n"
+        self.assertEqual(response, expected)
+
+    def test_check_vitals(self): 
+        response = patientrepositry.patientCheckVitals()
         expected = ''
-        self.assertEqual(response,expected)
-    
+        self.assertEqual(response, expected)
+
     def test_discharge_patient(self):
-        data =  {"bedid" : "1"}
+        data = {"bedid": "1"}
         bedid = data["bedid"]
-        response=patientrepositry.dischargePatient(bedid);
+        response = patientrepositry.dischargePatient(bedid)
         # expected = "Patient Suresh is discharged"
         expectedd = ' '
-        self.assertEqual(response,expectedd)
-        
-
+        self.assertEqual(response, expectedd)
+    
+    
 if __name__ == '__main__' :
     unittest.main()
